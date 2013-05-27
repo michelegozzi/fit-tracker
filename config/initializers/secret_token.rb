d820aa461e2be7bd4fd1386799519eb25087d675
Michele Gozzi <michelegozzi@gmail.com>
2013-05-27 18:43:11 +0200

    welcome fit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitTracker::Application.config.secret_token = '6190da72ae13a425b7043b952eaa260a07f2b780ac14ba2a688bf2f34a2d287b05eb74007ddb93cab8662bba99159d35757effa4d645facfbff7e1f39571e569'
