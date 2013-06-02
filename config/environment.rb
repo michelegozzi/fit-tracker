1f00ef5a1057eefdb9c0193ee8bfa05613799c97
Michele Gozzi <michelegozzi@gmail.com>
2013-06-02 14:34:55 +0200

    deprecation silenced

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 FitTracker::Application.initialize!
+
+if ENV['RAILS_ENV'] == "production"
+  ActiveSupport::Deprecation.silenced = true
+end
